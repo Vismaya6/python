868d84f0e9d9f8ff4d3cf6212a503806dadfee6d
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-10-05 15:50:30 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,9 @@
+a=10
+b=5
+sum=a+b
+Division=a/b
+print("sum:",sum,",Division:",Division)
+print("Is a greater than b?:",a>b)
+print("Are a and b equal?:",a==b)
+print("Logical AND:",a>b and b<a)
+print("Logical OR:",a>b or b>a)
