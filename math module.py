09661753267385436eb5a0c2028cf7148006058a
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-10-05 15:49:47 +0530

    Add files via upload

---
+++ math module.py
@@ -0,0 +1,5 @@
+import math
+no=int(input("Enter a number:"))
+print("Square root of",no,":",math .sqrt(no))
+print("Factorial of",no,":",math.factorial(no))
+print(no,"raised to number 2:",math.pow(no,2))
