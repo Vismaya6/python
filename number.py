72438321bd49506c5facb92e2d4565fdee2db97f
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-10-08 15:25:52 +0530

    Add files via upload

---
+++ number.py
@@ -0,0 +1,7 @@
+'''
+Author:Vismaya Theresa Benny
+version=1.0
+Program to print three numbers '''
+Number_1=int(input("Enter the First Number:"))
+Number_2=int(input("Enter the Second number:"))
+Number_3=int(input("Enter the Third number:"))
