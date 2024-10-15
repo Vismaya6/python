309fa59000914bcfec55145d80ce40839cb162df
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-10-15 14:11:52 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,13 @@
+'''
+Author:Vismaya Theresa Benny
+date:15/10/2024
+Python program to check whether the given number is positive or not
+'''
+number=int(input("Enter the number:"))
+if number>0:
+    print("The given number:",number,"is positive")
+elif number<0:
+    print("The given number:",number, "is negative")
+else:
+    print("The given number",number,"is zero")
+
