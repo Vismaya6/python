b0f983d342983ee04cd362aaed54cd3e17083de6
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-10-15 16:11:12 +0530

    Add files via upload

---
+++ odd no.py
@@ -0,0 +1,10 @@
+'''
+Author:Vismaya Theresa Benny
+date:15/10/2024
+Python program to print odd numbrs
+'''
+limit=int(input("Enter the limit:"))
+odd_number=1
+while odd_number<limit:
+    print(odd_number,"\t",end=" ")
+    odd_number+=2
