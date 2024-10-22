196cadf4c3c854cd66b94b0a8b579fb2dad12800
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-10-22 14:31:12 +0530

    Add files via upload

---
+++ if largest of three no's.py
@@ -0,0 +1,14 @@
+'''
+Author:Vismaya Theresa Benny
+Date:22/10/2024
+python program to find largest of three number"s
+'''
+No1=int(input("Enter the first no:"))
+No2=int(input("Enter the second no:"))
+No3=int(input("Enter the third no:"))
+if (No1>No2 and No1>No3):
+    print("The largest number is:",No1)
+elif(No2>No3):
+    print("The largest number is",No2)
+else:
+    print("Largest number is:",No3)
