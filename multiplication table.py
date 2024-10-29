57b73adaef443cab36d9bf08ef7949d24c353184
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-10-29 15:26:44 +0530

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,11 @@
+'''
+Author:Vismaya Theresa Benny
+Date:29/10/2024
+Program to print multiplication table upto 12
+'''
+Number=int(input("Enter a number:"))
+for i in range (1,13):
+    table=i*Number
+    print(f"{i} *{Number} ={table}")
+
+
